93988e67dafaf245d9cc381628c778f3abb3f57d
harsh199823 <168401431+harsh199823@users.noreply.github.com>
2024-05-08 13:20:36 +0530

    Update settings.py
    removing rest_framework

--- chat_bot/settings.py
+++ chat_bot/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'rest_framework',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pro',
